76e91b0cae0eef866df3ca81107a17a3501d07a9
floriangrell <florian@hauptsache.net>
2018-03-23 16:45:40 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+  'title' => 'Floating Action Button eXtension',
+  'descriptions' => '',
+  'category' => 'fe',
+  'constraints' => 
+  [
+    'depends' => 
+    [
+    ],
+    'conflicts' => 
+    [
+    ],
+    'suggests' => 
+    [
+    ],
+  ],
+  'state' => 'alpha',
+  'uploadfolder' => false,
+  'createDirs' => '',
+  'clearCacheOnLoad' => true,
+  'author_company' => 'Hauptsache.net GmbH',
+];
