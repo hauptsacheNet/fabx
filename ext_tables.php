a26582f2750dad5547052bd4e8f5691d6a238dc4
floriangrell <florian@hauptsache.net>
2018-03-26 11:15:31 +0200

    Basic Typoscript includes and setup

--- ext_tables.php
+++ ext_tables.php
@@ -2,3 +2,5 @@
 defined('TYPO3_MODE') or die();
 
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('fabx', 'Configuration/TypoScript', 'fabx configuration');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('fabx', 'Configuration/TypoScript/IncludeInPage', 'fabx include in page');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('fabx', 'Configuration/TypoScript/DefaultResources', 'fabx default resources');
