0a97b7da51b3aedbea73a7ba1207d36b63101e4d
floriangrell <florian@hauptsache.net>
2018-03-27 17:55:20 +0200

    Add tx_fabx_action to rootline

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,9 @@
 defined('TYPO3_MODE') or die();
 
 TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:fabx/Configuration/TSconfig/tsconfig.txt">');
+
+if(empty($GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] = 'tx_fabx_action';
+} else {
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_fabx_action';
+}
