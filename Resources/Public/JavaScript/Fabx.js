775f405a577951b17dc5fb0f3364be532dc7af63
floriangrell <florian@hauptsache.net>
2018-03-28 12:00:47 +0200

    Refactored Fabx Javascript to an jQuery plugin

--- Resources/Public/JavaScript/Fabx.js
+++ Resources/Public/JavaScript/Fabx.js
@@ -1,39 +1,94 @@
-(function($) {
+;(function($, window, document, undefined) {
 
-    var $document,
-        $container,
-        $toggleButton,
-        $actionContainer;
-
-    $(function() {
-
+    var pluginName = 'fabx',
+        defaults = {
+            toggleButtonClass: '.fabx__toggle',
+            actionContainerClass: '.fabx__actions',
+            activeClass: '.fabx--is-open',
+            buttonClass: '.fabx__btn'
+        },
         $document = $(document);
-        $container = $('.fabx');
-        $toggleButton = $('.fabx__toggle');
-        $actionContainer = $('.fabx__actions');
 
-        var closeContainer = function() {
-            $container.removeClass('fabx--is-open');
-            $document.off('keyup.fabx');
-        };
+    function Plugin(element, options) {
+        this.element = element;
+        this.options = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
 
-        $toggleButton.on('click.fabx', function(e) {
-            e.preventDefault();
-            if($container.hasClass('fabx--is-open')) {
-                closeContainer();
+        this.init();
+    }
+
+    Plugin.prototype = {
+
+        init: function() {
+
+            this.$element = $(this.element);
+            this.$actionContainer = $(this.options.actionContainerClass);
+
+            this.generateClassNames();
+            this.attachEventListeners();
+
+        },
+
+        generateClassNames: function() {
+            this.options.activeClassName = this.options.activeClass.substr(1);
+        },
+
+        attachEventListeners: function() {
+            var that = this;
+            this.$element.find(this.options.toggleButtonClass).on('click.' + pluginName, function(e) {
+               e.preventDefault();
+               that.toggleContainer();
+            });
+
+            this.$actionContainer.on('click.' + pluginName, this.options.buttonClass, function(e) {
+                that.$actionContainer.find(that.options.activeClass).removeClass(that.options.activeClassName);
+                $(this).closest('li').addClass(that.options.activeClassName);
+            });
+        },
+
+        toggleContainer: function() {
+            if(this.$element.hasClass(this.options.activeClassName)) {
+                this.closeContainer();
             } else {
-                $container.addClass('fabx--is-open');
-                $document.on('keyup.fabx', closeContainer);
-                if($container.find('.fabx--is-open').length === 0) {
-                    $container.find('li:first-child').addClass('fabx--is-open');
-                }
+                this.openContainer();
             }
+        },
+
+        openContainer: function() {
+            this.$element.addClass(this.options.activeClassName);
+
+            $document.on('keyup.' + pluginName, this.closeContainer.bind(this));
+
+            this.openFirstActionIfNoneSelected();
+
+        },
+
+        closeContainer: function() {
+            this.$element.removeClass(this.options.activeClassName);
+            $document.off('keyup.' + pluginName);
+        },
+
+        openFirstActionIfNoneSelected: function() {
+            if(this.$actionContainer.find(this.options.activeClass).length === 0) {
+                this.$actionContainer.find('> :first-child').addClass(this.options.activeClassName);
+            }
+        }
+
+    };
+
+    $.fn[pluginName] = function(options) {
+
+        return this.each(function() {
+
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+            }
+
         });
 
-        $actionContainer.on('click.fabx', '.fabx__btn', function(e) {
-            $actionContainer.find('.fabx--is-open').removeClass('fabx--is-open');
-            $(this).closest('li').addClass('fabx--is-open');
-        });
-    });
+    };
 
-})(jQuery);
+})(jQuery, window, document);
+
+
